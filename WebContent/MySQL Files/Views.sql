a872603d332a84f7726259cf78e33efe96179982
QuickHawk <devarlaaarya@gmail.com>
2020-03-09 17:20:00 +0530

    MySQL View Update

--- WebContent/MySQL Files/Views.sql
+++ WebContent/MySQL Files/Views.sql
@@ -5,14 +5,14 @@ drop view if exists `Book_Description`;
 
 create view `Book_Description` as
 	select 
-		book.id 			as `Book ID`,
-        book.name 			as `Book Name`,
+		book.id 			as `Book_ID`,
+        book.name 			as `Book_Name`,
         book.author 		as `Author`,
         book.date_added 	as `Date_Added`,
         book.available		as `Is_Available`,
         book.photo_location as `Photo_Location`,
-        location.floor 		as `Floor No`,
-        location.room 		as `Room No`,
+        location.floor 		as `Floor_No`,
+        location.room 		as `Room_No`,
         location.section 	as `Section`,
         location.shelf 		as `Shelf`,
         (select group_concat(tag) from tags where book_id = book.id) as `Tags`
